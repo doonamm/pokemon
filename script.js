b0575bacca6a52abafad4160bef4cfef7ab3d80c
Nam Ant <83125103+doonamm@users.noreply.github.com>
2021-09-24 18:01:27 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,79 @@
-setInterval(createSnowFlake, Math.floor(Math.random()*100) + 70)
+const poke_container = document.getElementById('poke-container');
+const poke_number = 200;
+const colors = {
+    fire: '#fddfdf',
+    grass: '#defde0',
+    electric: '#fcf7de',
+    water: '#def3fd',
+    ground: '#f4e7da',
+    rock: '#d5d5d4',
+    fairy: '#fceaff',
+    poison: '#98d7a5',
+    bug: '#f8d5a3',
+    dragon: '#97b3e6',
+    psychic: '#eaeda1',
+    flying: '#f5f5f5',
+    fighting: '#e6e0d4',
+    normal: '#f5f5f5'
+}
+const main_type = Object.keys(colors);
 
-var isFullScreen = false;
+fetchPokes();
 
-document.addEventListener('dblclick', ()=>{
-    if(!isFullScreen){
-        document.documentElement.requestFullscreen();
+async function fetchPokes () {
+    for (let i = 1; i <= poke_number; i++) {
+        await getPokeCard(i);
     }
-    else
-        document.exitFullscreen();
-    isFullScreen = !isFullScreen;
-})
+}
 
-function createSnowFlake(){
-    const snow_flake = document.createElement('i');
-    const font_size = window.getComputedStyle(document.querySelector('.fa-snowflake')).getPropertyValue('font-size');
+async function getPokeCard (id) {
+    try{
+        const url = getDataUrl(id);
+        const res = await fetch(url);
+        const dataJson = await res.json();
+        createCard(dataJson);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
 
-    snow_flake.classList.add('fas', 'fa-snowflake');
+function createCard(data){
+    const card = document.createElement('div');
+    const types = data.types.map(o => o.type.name);
+    const poke_type = main_type.find(type => types.indexOf(type) > -1);
+    const poke_id = data.id.toString().padStart(3, '0');
+    const poke_url = getImageUrl(poke_id);
 
-    const randomTime = Math.round(Math.random()*5) + 4;
+    card.style.background = colors[poke_type];
+    card.className = 'pokemon';
+    card.innerHTML = createHTMLCard({
+        name: data.name,
+        type: poke_type,
+        id: poke_id,
+        url: poke_url,
+    });
+    
+    poke_container.appendChild(card);
+}
 
-    snow_flake.style.left = Math.random()*document.body.clientWidth + 'px';
-    snow_flake.style.animationDuration = randomTime + 's';
-    snow_flake.style.opacity = Math.random();
-    snow_flake.style.fontSize = Math.round(Math.random()*10) - 5 + parseInt(font_size) + 'px';
+function createHTMLCard(data){
+    return `
+        <div class="img-container">
+            <img loading="lazy" src=${data.url} width="108px" height="108px">
+        </div>
+        <div class="info">
+            <span class="number">#${data.id}</span>
+            <h3 class="name">${data.name}</h3>
+            <small class="type">Type: <span>${data.type}</span></small>
+        </div>
+    `;
+}
 
-    document.body.appendChild(snow_flake);
-    console.log(document.body.offsetWidth);
-    setTimeout(()=>{
-        snow_flake.remove();
-    }, randomTime*1000);
+function getDataUrl(id){
+    return `https://pokeapi.co/api/v2/pokemon/${id}`;
+}
+
+function getImageUrl(id){
+    return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id}.png`;
 }
